7ccfec974ece8623f63bd240b9a4e6706a173f11
heejae <gmlwo0109@gmail.com>
2019-03-07 16:20:42 +0900

    first commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -130,6 +130,35 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio','static')
 ]#static 파일들이 현재 어디에 있는지를 쓰는 곳
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# Debug ToolBar
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+}
+
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # static 파일들이 어디로 모일 것인지를 쓰는 곳, static폴더로 모을거야!
 
